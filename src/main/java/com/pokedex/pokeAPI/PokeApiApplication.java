31ff5653c940127ddfd267d134a07e3115c59f05
Wessel Scholtz <wessels@bbd.co.za>
2022-06-13 02:28:07 +0200

    Evo First

--- src/main/java/com/pokedex/pokeAPI/PokeApiApplication.java
+++ src/main/java/com/pokedex/pokeAPI/PokeApiApplication.java
@@ -8,11 +8,18 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.lang.invoke.MethodHandles;
+
 @Configuration
 @SpringBootApplication
 public class PokeApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass()); // as per documentation https://www.slf4j.org/faq.html#declared_static
+
 	public static void main(String[] args) {
+		logger.info("Starting up...");
 		SpringApplication.run(PokeApiApplication.class, args);
 	}
 
---
+++ src/main/java/com/pokedex/pokeAPI/services/EvolutionService.java
@@ -0,0 +1,58 @@
+package com.pokedex.pokeAPI.services;
+
+import com.pokedex.pokeAPI.models.rest.response.ResponseEvolutionPokemon;
+import com.pokedex.pokeAPI.repositories.PokemonRepository;
+import com.pokedex.pokeAPI.repositories.PokemonSpeciesRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.pokedex.pokeAPI.models.data.PokemonData;
+import com.pokedex.pokeAPI.models.data.PokemonSpeciesData;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service("EvoSrv")
+@Slf4j
+public class EvolutionService {
+
+    @Autowired
+    PokemonRepository pokemonRepo;
+    
+    @Autowired
+    PokemonSpeciesRepository pokemonSpeciesRepo;
+
+    public List<ResponseEvolutionPokemon> listEvolutions(String pokemonIdentifier) {
+        log.info("Request for {}", pokemonIdentifier);
+        var request = pokemonRepo.findByidentifier(pokemonIdentifier);
+
+        if (request.size() == 0) return Collections.emptyList();
+
+        var requestPokemon = request.get(0);
+
+        var requestPokemonS = requestPokemon.getPokemon_species();
+        
+        var requestPokemonFamily = pokemonSpeciesRepo.getFamily(requestPokemonS.getEvolution_chain_id());
+
+        requestPokemonFamily.sort((sp1, sp2) -> sp1.getOrder().compareTo(sp2.getOrder()));             
+
+        List<ResponseEvolutionPokemon> ret = new ArrayList<ResponseEvolutionPokemon>();
+        for (int i = 0; i < requestPokemonFamily.size(); i++) {
+            var pokeSpecies = requestPokemonFamily.get(i);
+            var pokes = pokeSpecies.getBasePokemon();
+            for (PokemonData p : pokes) {
+                ret.add(new ResponseEvolutionPokemon(p.getIdentifier(), i == 0, i));
+            } 
+        }
+
+        log.info("Response from {} request was: {}", pokemonIdentifier, ret.toString());
+        return ret;
+       
+        //CANT DO STREAM CUS I NEED NUMBERS
+        //.flatMap(ps -> ps.getBasePokemon().stream()).map(p -> new ResponseEvolutionPokemon(p.getIdentifier(), rootChain, chainPosition)).Collect(Collectors.toList());
+    }
+}
