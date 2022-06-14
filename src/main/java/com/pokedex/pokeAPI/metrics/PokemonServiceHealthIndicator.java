7d888f09cd8d0197d82f0f87755b2d82cad8acb7
Jacus Horne <jacus@bbd.co.za>
2022-06-14 14:22:38 +0200

    added custom metric skeleton

--- src/main/java/com/pokedex/pokeAPI/metrics/PokemonServiceHealthIndicator.java
+++ src/main/java/com/pokedex/pokeAPI/metrics/PokemonServiceHealthIndicator.java
@@ -1,5 +1,14 @@
 package com.pokedex.pokeAPI.metrics;
 
-public class PokemonServiceHealthIndicator {
-    
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PokemonServiceHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        return Health.up().withDetail("very nice", true).build();
+    }
 }
