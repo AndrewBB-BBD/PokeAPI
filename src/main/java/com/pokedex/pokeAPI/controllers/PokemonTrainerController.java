2c78a0fc7ba16592896dbb73f319daea1718744f
Chadley Maggott <chadleym@bbd.co.za>
2022-06-14 15:28:53 +0200

    documentation

--- src/main/java/com/pokedex/pokeAPI/controllers/PokemonTrainerController.java
+++ src/main/java/com/pokedex/pokeAPI/controllers/PokemonTrainerController.java
@@ -1,12 +1,18 @@
 package com.pokedex.pokeAPI.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pokedex.pokeAPI.models.AuthDetails;
 import com.pokedex.pokeAPI.models.Pokemon;
 import com.pokedex.pokeAPI.models.TamePokemon;
+import com.pokedex.pokeAPI.models.rest.response.ResponseEvolutionPokemon;
 import com.pokedex.pokeAPI.repositories.TrainerRepository;
 import com.pokedex.pokeAPI.security.JwtUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +30,25 @@ public class PokemonTrainerController {
     @Autowired
     TrainerRepository trainerRepository;
 
-    @Operation(summary = "Upload new pokemon you caught", description = "Caught a new pokemon? Save it to your user profile!")
+    @Operation(
+            summary = "Upload your pokemon",
+            description = "Caught a new pokemon? Upload load it to your user account.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Upload is successful",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = String.class))
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view this endpoint"),
+                    @ApiResponse(responseCode = "403", description = "The endpoint you were trying to reach is forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Endpoint not found")
+            }
+    )
     @PutMapping("/uploadPokemonCaught")
-    public ResponseEntity<Object> uploadPokemonCaught(@RequestHeader String authorization, @RequestBody Pokemon pokemon,
+    public ResponseEntity<String> uploadPokemonCaught(@RequestHeader String authorization, @RequestBody Pokemon pokemon,
                                                       @RequestParam String nickname) throws JsonProcessingException {
         String user = jwtUtil.decodeUsername(authorization);
         TamePokemon tamePokemon = new TamePokemon(user, nickname, pokemon);
@@ -38,8 +60,23 @@ public class PokemonTrainerController {
         }
     }
 
-    @Operation(summary = "Get your pokemon/best-friend", description = "Get all your pokemon or an individual " +
-            "by calling them by name")
+    @Operation(
+            summary = "Get your pokemon/best-friend",
+            description = "Get all your pokemon or an individual by calling them by name",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful get all your pokemon",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = String.class))
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "You are not authorized to view this endpoint"),
+                    @ApiResponse(responseCode = "403", description = "The endpoint you were trying to reach is forbidden"),
+                    @ApiResponse(responseCode = "404", description = "Endpoint not found")
+            }
+    )
     @PostMapping("/getMyPokemon")
     public ResponseEntity<Object> getMyPokemon(@RequestHeader String authorization,
                                                @RequestParam Optional<String> pokemonName) throws JsonProcessingException {
